1ce6e02427eb02eff27287ea26ce5f245d7874b3
Imon <contact@imonweb.co.uk>
2022-03-29 17:46:00 +0100

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+import { getAnalytics } from "firebase/analytics";
+
+// import * as firebase from 'firebase/app';
+// import 'firebase/storage';
+// import 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBtDfyLrct7DsddXPnB-lzIirSqHDkvWrg",
+  authDomain: "firegram-imon.firebaseapp.com",
+  projectId: "firegram-imon",
+  storageBucket: "firegram-imon.appspot.com",
+  messagingSenderId: "637647772807",
+  appId: "1:637647772807:web:13ab5988a0aa732046ac22",
+  measurementId: "G-02WRE6GP29"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+initializeApp(firebaseConfig);
+ 
+
+// firebase.initializeApp(firebaseConfig);
+// const projectStorage = firebase.storage();
+const projectStorage = getStorage();
+// const projectFirestore = firebase.firestore();
+const projectFirestore = getFirestore();
+
+export { projectStorage, projectFirestore };
---
+++ src/hooks/useStorage.js
@@ -0,0 +1,31 @@
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import React, {useState, useEffect} from 'react'
+import {projectStorage, projectFirestore} from '../firebase/config'
+ 
+
+const useStorage = (file) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+  
+
+  useEffect(() => {
+    // references
+    const storageRef = projectStorage.ref(file.name)
+    // const collectionRef = ref(projectStorage, "images/" + file.name);
+    const collectionRef = projectFirestore.collection('images');
+
+    storageRef.put(file).on('state_changed', (snap) => {
+      let percentage = (snap.bytesTrasnferred / snap.totalBytes) * 100;
+      setProgress(percentage);
+    }, (err) => {
+      setError(err)
+    }, async () => {
+      const url = await storageRef.getDownloadURL();
+      setUrl(url);
+    })
+  }, [file]) 
+  return ( progress, url, error )
+}
+
+export default useStorage;
